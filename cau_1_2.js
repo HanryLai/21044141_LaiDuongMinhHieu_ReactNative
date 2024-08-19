57f69fc710e5c16ea1b9d27fdc3ffbba9c154456
Hieu Lai <ldmhieudev@gmail.com>
2024-08-19 16:03:36 +0700

    Feat: Cau 2 template string

--- cau_1_2.js
+++ cau_1_2.js
@@ -1,3 +1,4 @@
+//cau 1
 // § Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
 // m tall.
 // § Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
@@ -18,3 +19,23 @@ const john2BMI = BMICal(john2.weight, john2.tall);
 
 console.log("data 1: ", mark1BMI, "\n", john1BMI, "\n");
 console.log("data 2: ", mark2BMI, "\n", john2BMI, "\n");
+
+// cau 2
+// Print a nice output to the console, saying who has the higher BMI. The message
+// is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+// 2. Use a template literal to include the BMI values in the outputs. Example: "Mark's
+// BMI (28.3) is higher than John's (23.9)!"
+const cau2_print = (mark, join) => {
+    if (mark > join) {
+        console.log(`Mark's BMI (${mark}) is higher than John's (${join})!`);
+    } else {
+        console.log(`John's BMI (${join}) is higher than Mark's (${mark})!`);
+    }
+};
+
+console.log("cau 2: ");
+console.log("data 1: ");
+cau2_print(mark1BMI, john1BMI);
+console.log("data 2: ");
+
+cau2_print(mark2BMI, john2BMI);
