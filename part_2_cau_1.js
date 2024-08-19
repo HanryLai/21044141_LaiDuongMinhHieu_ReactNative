74d4dc4e552181fd1c987fc5f2504c12210ae308
Hieu Lai <ldmhieudev@gmail.com>
2024-08-19 16:35:37 +0700

    Feat: Bai 1 phan 2

--- part_2_cau_1.js
+++ part_2_cau_1.js
@@ -1,6 +1,20 @@
-const Dolphins = [44, 23, 71];
-const Koalas = [65, 54, 49];
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
 
-const average = (arr) => {
-    return arr.reduce((acc, cur) => acc + cur, 0) / arr.length;
+const avgDolphins1 = calcAverage(44, 23, 71);
+const avgKoalas1 = calcAverage(65, 54, 49);
+
+const avgDolphins2 = calcAverage(85, 54, 41);
+const avgKoalas2 = calcAverage(23, 34, 27);
+
+const checkWinner = (avgDolphins, avgKoalas) => {
+    if (avgDolphins >= 2 * avgKoalas) {
+        console.log(`Dolphins thắng (${avgDolphins} vs. ${avgKoalas})`);
+    } else if (avgKoalas >= 2 * avgDolphins) {
+        console.log(`Koalas thắng (${avgKoalas} vs. ${avgDolphins})`);
+    } else {
+        console.log("Không có đội nào thắng!");
+    }
 };
+
+checkWinner(avgDolphins1, avgKoalas1); // Kiểm tra dữ liệu 1
+checkWinner(avgDolphins2, avgKoalas2); // Kiểm tra dữ liệu 2
